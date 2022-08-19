093a3303eee186131e6cae610d233d87ac2b39b8
Julian Kraemer <juliankraemer184@gmail.com>
2022-08-19 12:23:07 +0200

    broken phong

--- src/phong-vertex-shader.glsl
+++ src/phong-vertex-shader.glsl
@@ -2,6 +2,7 @@ precision mediump float;
 
 attribute vec3 a_position;
 attribute vec3 a_normal;
+attribute vec4 a_color;
 
 // Pass color as attribute and forward it
 // to the fragment shader
@@ -12,6 +13,8 @@ uniform mat4 V; // View matrix
 uniform mat4 N; // normal matrix
 
 varying vec3 v_normal;
+varying vec4 v_color;
+varying vec4 v_position;
 
 // Pass the vertex position in view space
 // to the fragment shader
@@ -22,6 +25,7 @@ void main() {
   
   // Pass the color and transformed vertex position through
   // TODO
-  
+  v_position = V * M * vec4(a_position, 1.0);
+  v_color = a_color;
   v_normal = normalize((V * N * vec4(a_normal, 0)).xyz);
 }

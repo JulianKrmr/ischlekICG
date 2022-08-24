7a9be904756ef6961c80fa0c69104ea952a867c1
Julian Kraemer <juliankraemer184@gmail.com>
2022-08-24 09:03:02 +0200

    exercise 09 perspective shader changes

--- src/perspective-vertex-shader.glsl
+++ src/perspective-vertex-shader.glsl
@@ -4,7 +4,20 @@ uniform mat4 M;
 uniform mat4 V;
 uniform mat4 P;
 
+attribute vec3 a_normal;
+attribute vec4 a_color;
+
+uniform mat4 N; // normal matrix
+
+varying vec3 v_normal;
+varying vec4 v_color;
+varying vec4 v_position;
+
+
 void main() {
   gl_Position = P*V * M*vec4(a_position, 1.0);
   // TODO
+  v_position = V * M * vec4(a_position, 1.0);
+  v_color = a_color;
+  v_normal = normalize((V * N * vec4(a_normal, 0)).xyz);
 }
